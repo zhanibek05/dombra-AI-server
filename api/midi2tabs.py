f52537572636101f3354361e28265f85b9e6e25b
zhanibek05 <122998214+zhanibek05@users.noreply.github.com>
2024-07-23 15:08:46 +0500

    change path of midi file

--- api/midi2tabs.py
+++ api/midi2tabs.py
@@ -6,10 +6,10 @@ def midi2tabs(file_path):
 
     dombyra_tabs = {}
     model_output, midi_data, note_events = predict(file_path)
-    midi_data.write('C:/Users/Zhanibek/Desktop/dombraAI/backend/dombra_backend/media/output.mid')
+    midi_data.write('/home/ubuntu/dombra-AI-server/media/output.mid')
 
         
-    midi_data = pretty_midi.PrettyMIDI("C:/Users/Zhanibek/Desktop/dombraAI/backend/dombra_backend/media/output.mid")
+    midi_data = pretty_midi.PrettyMIDI("/home/ubuntu/dombra-AI-server/media/output.mid")
   
 
     temp_tabs = []
